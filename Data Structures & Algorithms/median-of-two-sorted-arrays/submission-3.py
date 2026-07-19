9d85c45b665709bd868701fe218b58a2e2b4ca6a
Sharan Krishna <87695707+SilverXer0@users.noreply.github.com>
2026-07-19 16:11:31 -0700

    Add: median-of-two-sorted-arrays - submission-3

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A = nums1
+        B = nums2
+        res = 0
+        half = (len(A) + len(B)) // 2
+
+        if len(A) > len(B):
+            A, B = B, A
+
+        l = 0
+        r = len(A) - 1
+
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+
+            ALeft = A[i] if i >= 0 else float('-inf')
+            ARight = A[i+1] if i + 1 < len(A) else float('inf')
+            BLeft = B[j] if j >= 0 else float('-inf')
+            BRight = B[j + 1] if j + 1 < len(B) else float('inf')
+
+            if ALeft <= BRight and BLeft <= ARight:
+                if (len(A) + len(B)) % 2 == 1:
+                    res = min(ARight, BRight)
+                    return res
+                else:
+                    res = (max(ALeft, BLeft) + min(ARight, BRight)) / 2
+                    return res
+            elif ALeft > BRight:
+                r = i - 1
+            else:
+                l = i + 1
